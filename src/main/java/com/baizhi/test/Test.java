ae9720518215ba079aa81ebb57b96bcd6e1372e3
htf <2889921390@qq.com>
2018-10-19 14:26:36 +0800

    this is dev commit3

--- src/main/java/com/baizhi/test/Test.java
+++ src/main/java/com/baizhi/test/Test.java
@@ -7,6 +7,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("我是dev分支");
         System.out.println("我是dev分支");
+        System.out.println("我是dev分支");
     }
     
     
